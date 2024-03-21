9a751a3d3f159fc95f774b2b1982cf7ab92ce0a0
misterclayt0n <daviarantes13@gmail.com>
2024-03-21 13:17:51 -0300

    add support for window rezise

--- main.go
+++ main.go
@@ -9,10 +9,7 @@ func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		log.Fatalf("Erro ao inicializar SDL: %s", err)
 	}
-	defer sdl.Quit()
-
-	window, err := sdl.CreateWindow("Pong em Go com SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
+	defer sdl.Quit() window, err := sdl.CreateWindow("Pong em Go com SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN|sdl.WINDOW_RESIZABLE) if err != nil {
 		log.Fatalf("Erro ao criar janela: %s", err)
 	}
 	defer window.Destroy()
@@ -25,16 +22,18 @@ func main() {
 
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
+			switch e := event.(type) {
 			case *sdl.QuitEvent:
 				return
+			case *sdl.WindowEvent:
+				if e.Event == sdl.WINDOWEVENT_RESIZED {
+					renderer.SetViewport(&sdl.Rect{X: 0, Y: 0, W: e.Data1, H: e.Data2})
+				}
 			}
 		}
 
 		renderer.SetDrawColor(255, 0, 0, 255)
-
 		renderer.Clear()
-
 		renderer.Present()
 	}
 }
